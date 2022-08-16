48e2f4dc77472785f1c1d488a39e5fe8e2f698a3
kenkomu <komukenneth216@gmail.com>
2022-08-16 22:36:49 +0300

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,22 +10,22 @@
 void bubble_sort(int *array, size_t size)
 {
 	bool swapped = false;
-	size_t i, j;
+	size_t  j;
 	int temp;
 
 	do {
 		swapped = false;
-		for (int j = 0; j < (size - 1 - i); j++)
+		for (j = 0; j < (size - 1 ); j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				int temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-				printf("a[%lu] = %d\n", i,array[i]);
+				printf("a[%lu] = %d\n", j,array[j]);
 				swapped = true;
 			}
 		}
-		i++;
+		j++;
 	} while (swapped);
 }
