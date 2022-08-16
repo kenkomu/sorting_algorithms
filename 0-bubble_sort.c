08b5394ef8bc1b8a9e9312ad4e6fa2457293ee4e
kenkomu <komukenneth216@gmail.com>
2022-08-16 22:10:46 +0300

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,7 +1,39 @@
 #include "sort.h"
+#include <stdbool.h>
 /**
  *  function that sorts an array of integers in ascending order
  *  using the Bubble sort algorithm
  * 
  * 
  */
+int main(void)
+{
+
+	int array[] = {};
+
+	int size = 10;
+
+	return (0);
+}
+
+void bubble_sort(int *array, size_t size)
+{
+	bool swapped = false;
+	int i = 0;
+
+	do {
+		swapped = false;
+		for (int j = 0; j < (size - 1 - i); j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				int temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				printf("a[%d] = %d\n", i,array[i]);
+				swapped = true;
+			}
+		}
+		i++;
+	} while (swapped);
+}
