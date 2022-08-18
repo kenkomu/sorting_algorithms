f8ef2916b82ade9f4150a943c7b7113654fa7401
kenkomu <komukenneth216@gmail.com>
2022-08-18 15:48:27 +0300

    selection_sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,26 +1,42 @@
 #include "sort.h"
-#include <stdio.h>
 /**
- * bubble_sort - sorts an array in ascending order
- * @array: array of ints to sort
- * @size: size of the array
+ * swap - helper function
+ * Return: void
+ * @a: int ptr
+ * @b: int ptr
+ * Description: Swap two elements in an array
  */
-void bubble_sort(int *array, size_t size)
+void swap(int *a, int *b)
 {
-        unsigned int i, j;
-        int tmp;
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+/**
+ * selection_sort - selection sorting function
+ * Return: void
+ * @array: int ptr
+ * @size: size_t
+ * Description: Selection sorting function
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min_i;
 
-        if (size < 2)
-                return;
+	if (!array || size < 2)
+		return;
 
-        for (i = 0;i < size; i++){
-                for (int j = 0; j < size - i;j++){
-                        if (array[j] > array[j + 1]){
-                                int tmp = array[j];
-                                array[j + 1] = array[j];
-                                array[j + 1] = tmp;
-                                print_array(array, size);
-                        }
-                }
-        }
+	for (i = 0; i < size - 1; i++)
+	{
+		min_i = i;
+		for (j = i + 1; j < size; j++)
+			if (array[j] < array[min_i])
+				min_i = j;
+
+		if (i < min_i)
+		{
+			swap(&array[min_i], &array[i]);
+			print_array(array, size);
+		}
+	}
 }
