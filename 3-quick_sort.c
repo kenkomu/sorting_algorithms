17c91d8e8f15d4d2d345dc3b328a374f3e156432
kenkomu <komukenneth216@gmail.com>
2022-08-18 12:25:38 +0300

    Quick_sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+/**
+ * quick_sort - function that sorts an array of integers
+ * in ascending order using the Quick sort algorithm
+ * @array: array of ints to sort
+ * @size: size of the array
+ */
+void quick_sort(int *array, size_t size){
+    int i,j,temp
+    
+}
