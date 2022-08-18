2237f5db8257293bffeeedb1291e5309ca50e2a2
kenkomu <komukenneth216@gmail.com>
2022-08-18 09:33:52 +0300

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,5 +1,4 @@
 #include "sort.h"
-#include <stdio.h>
 
 /**
  * bubble_sort - sorts an array in ascending order
@@ -14,16 +13,12 @@ void bubble_sort(int *array, size_t size)
 	if (size < 2)
 		return;
 
-	for (i = 0; i < size; i++)
-	{
-		for (j = 0; j < size - i - 1; j++)
-		{
-			if (array[j] > array[j + 1])
-			{
-				tmp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = tmp;
-				print_array(array, size);
+	for (i = 0;i < size; i++){
+		for (int j = 0; j < size - i;j++){
+			if array[j] > array[j + 1]{
+				int tmp = a[j]
+				array[j + 1] = array[j]
+				array[j + 1] = tmp
 			}
 		}
 	}
