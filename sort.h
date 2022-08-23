29852b085773a068ebfc3ec32e68c4d13bf9ee3f
kenkomu <komukenneth216@gmail.com>
2022-08-23 22:31:00 +0300

    header file

--- sort.h
+++ sort.h
@@ -27,5 +27,6 @@ void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
+void shell_sort(int *array, size_t size);
 
 #endif
